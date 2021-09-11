d422ec527494de0ae9e0722fe965dddb0b6f90ad
Jaehwi <iamhwii@gmail.com>
2021-09-11 18:44:44 +0900

    korail reservation loop

---
+++ korail.py
@@ -0,0 +1,61 @@
+from korail2 import *
+import requests
+import time as tt
+import datetime
+
+
+# get idpw
+login_dict = {}
+with open('./credentials/idpw', 'r') as f:
+    for l in f.readlines():
+        k, v = l.split()
+        login_dict[k] = v
+
+# get url
+url = ''
+with open('./credentials/slack', 'r') as f:
+    for l in f.readlines():
+        if 'URL' in l.split():
+            _, url = l.split()
+
+# define messeging func
+def msg_to_slcak(msg, url=url):
+    r = requests.post(
+            url,
+            json = {"text": msg},
+            headers = {'Content-type': 'application/json'}
+    )
+    print("posting msg result: ", r)
+# msg_to_slcak("slack api test")
+
+KR = Korail(login_dict['ID'], login_dict['PW'])
+dep = '용산'
+arr = '순천'
+date = '20210917'
+time = '180000'
+# time = '000000'
+
+state = True
+idx = 1
+while(state):
+    if idx % 360 == 0:
+        print(f"[{datetime.datetime.now()}] {date}, {time}, {dep} -> {arr} 열차표를 예매 중입니다...")
+        print(trains)
+    trains = KR.search_train(dep, arr, date, time, include_no_seats=True)
+    interesting_train = None
+    for train in trains:
+        if 'KTX' in train.__repr__():
+            if '원' in train.__repr__().split(' ')[-1]:
+                interesting_train = train
+                break
+    
+    if interesting_train:
+        rslt = KR.reserve(interesting_train, [AdultPassenger()])
+        if '구입기한' in rslt.__repr__().split(' '):
+            msg_to_slcak(rslt.__repr__())
+            state = False
+    tt.sleep(1)
+    idx += 1
+
+print(datetime.datetime.now())
+print("프로그램을 종료합니다!")
